38edb17b08549c0eebae1ebdf4930cb2f22bd443
Boas-World <84286574+Boas-World@users.noreply.github.com>
2021-08-04 04:36:27 +0200

    Add files via upload

--- app/src/main/java/com/Layton/paquet/etrange/dialogue1.java
+++ app/src/main/java/com/Layton/paquet/etrange/dialogue1.java
@@ -1,1 +1,29 @@
-1
+package com.Layton.paquet.etrange;
+
+import android.app.Activity;
+import android.content.*;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.widget.*;
+import com.Layton.paquet.etrange.R;
+import java.io.*;
+
+public class dialogue1 extends Activity{
+  
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        init();
+        
+    }
+    
+    private void init(){}
+    
+  
+}
+
+
